97f697f44c0812d4e3839721ab15fbd1e2d35def
Alexandre Vasconcelos Vicente <164345563+alevidev@users.noreply.github.com>
2026-09-17 23:22:40 -0300

    Teste das classes de personagens
    Testando classes e métodos no main.

--- Jogo/src/main/java/com/mycompany/jogo/Jogo.java
+++ Jogo/src/main/java/com/mycompany/jogo/Jogo.java
@@ -1,8 +1,49 @@
 package com.mycompany.jogo;
 
+import com.mycompany.jogo.personagens.Investigador;
+import com.mycompany.jogo.personagens.Suspeito;
+
 public class Jogo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Investigador investigador = new Investigador(
+                "Alexandre",
+                100,
+                100,
+                1,
+                20,
+                10,
+                15,
+                12,
+                10
+        );
+
+        Suspeito suspeito = new Suspeito(
+                "Ricardo",
+                80,
+                80,
+                1,
+                15,
+                8,
+                "Empresário",
+                "Eu nunca estive naquela estação.",
+                60,
+                70,
+                true
+        );
+
+        System.out.println("=== INVESTIGADOR ===");
+        investigador.mostrarStatus();
+
+        System.out.println("\n=== SUSPEITO ===");
+        suspeito.mostrarStatus();
+
+        System.out.println("\nRicardo está vivo?");
+        System.out.println(suspeito.estaVivo());
+
+        System.out.println("\nInvestigador ataca Ricardo!");
+        investigador.atacar(suspeito);
+
+        System.out.println("Vida de Ricardo: " + suspeito.getVida());
     }
 }
